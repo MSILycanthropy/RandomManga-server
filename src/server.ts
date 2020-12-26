b6658ddb2d2eced97be036117695a74a12356b92
MSILycanthropy <ethanmichaelk@gmail.com>
2020-12-25 19:19:45 -0600

    Testing for prod

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,14 @@
 import app from "./config/app";
 const PORT = 8000;
 
-var ip = process.env.INTERNAL_IP || "localhost";
+var ip: string;
+if (process.env.NODE_ENV == "production") {
+  ip = "192.168.0.22";
+} else {
+  ip = "localhost";
+}
 
 app.listen(PORT, ip, () => {
+  console.log(process.env.NODE_ENV);
   console.log(`Listening at http://${ip}:${PORT}`);
 });
