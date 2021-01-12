9151b25e1f0c205c2380e7e29ac464ea9fb0d935
MSILycanthropy <ethanmichaelk@gmail.com>
2021-01-11 22:25:55 -0600

    Adding report an error page

--- src/routes/email.routes.ts
+++ src/routes/email.routes.ts
@@ -1,10 +1,14 @@
 import { Application, Request, Response } from "express";
-import { sendMail } from "../logic/email";
+import { sendContactMail, sendReportMail } from "../logic/email";
 
 export class EmailRoutes {
   public route(app: Application) {
     app.post("/contact", (req: Request, res: Response) => {
-      sendMail(req, res);
+      sendContactMail(req, res);
+    });
+
+    app.post("/contact/report", (req: Request, res: Response) => {
+      sendReportMail(req, res);
     });
   }
 }
