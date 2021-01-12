1221b1c38337e0dac9e023ff36da41ac5b9d11b1
MSILycanthropy <ethanmichaelk@gmail.com>
2021-01-12 14:42:56 -0600

    Adding manga requests

--- src/logic/email.ts
+++ src/logic/email.ts
@@ -57,6 +57,51 @@ export function sendReportMail(req: Request, res: Response): void {
            Authors: ${req.body.authors} \n\
            Genres: ${req.body.genres} \n\
            Synopsis: ${req.body.synopsis} \n\
+           Other: ${req.body.other} \n\
+          `,
+  };
+
+  smtpTrans.sendMail(options, (err: any, res: Response) => {
+    if (!err) {
+      res.end("Thanks for the feedback!");
+    } else {
+      console.log(err);
+
+      res.end(`Something went wrong! \n ${err}`);
+    }
+  });
+}
+
+export function sendRequestMail(req: Request, res: Response): void {
+  const smtpTrans = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    auth: {
+      user: "RNGmanga.com@gmail.com",
+      pass: process.env.EMAIL_PASS,
+    },
+  });
+
+  const options = {
+    from: "Test",
+    to: "RNGmanga.com@gmail.com",
+    subject: `Manga Request from ${req.body.name}`,
+    text: `\tName: ${req.body.name} \n\
+           Email: ${req.body.email} \n\
+           Title: ${req.body.title} \n\
+           English Title: ${req.body.englishTitle} \n\
+           Alternate Titles: ${req.body.alternateTitles}\n\
+           Type: ${req.body.type} \n\
+           Volumes: ${req.body.volumes} \n\
+           Chapters: ${req.body.chapters} \n\
+           Start Date: ${req.body.startDate} \n\
+           End Date: ${req.body.endDate} \n\
+           Finished: ${req.body.finished} \n\
+           Authors: ${req.body.authors} \n\
+           Genres: ${req.body.genres} \n\
+           Synopsis: ${req.body.synopsis} \n\
+           Other: ${req.body.other} \n\
           `,
   };
 
